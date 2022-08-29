30c022cf266620a875183333f22d8265d3e101e1
Alexander <renyreny125tt@gmail.com>
2022-08-29 19:09:27 +0300

    feat: update cocollapsible

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,31 +1,82 @@
+const collapsible = document.querySelector('.collapsible');
 const button = document.querySelector('.collapsible__button');
+const visible = document.querySelector('.collapsible__action--visible');
+const hidden = document.querySelector('.collapsible__action--hidden');
 const content = document.querySelector('.collapsible__content');
 
-let collapsibleAnimation= [
-  {transform: 'translateY(0) rotateX(0deg)', opacity: '1'},
-  {transform: 'translateY(100px) rotateX(360deg)', opacity: '0'}
-]
+collapsible.style.width = '850px';
+collapsible.style.margin = '20px auto';
 
-let collapsibleOption = {
+const contentAnimation= [
+  {fontSize: '20px', opacity: '1', color: 'black'},
+  {fontSize: '25px', opacity: '1', color: 'green', offset: 0.4},
+  {fontSize: '1px', opacity: '0', color: 'red'},
+];
+const buttonAnimation = [
+  {transform: 'rotateX(0)'},
+  {transform: 'rotateX(360deg)'}
+];
+const buttonTextAnimation = [
+  {opacity: '1'},
+  {opacity: '0'}
+];
+
+const collapsibleOption = {
   duration: 1000
-}
+};
+
+button.style.width = '300px';
+button.style.height = '50px';
+button.style.cursor = 'pointer';
+content.style.fontSize = '20px';
+hidden.style.display = 'none';
 
 button.addEventListener('click', () => {
-  if (content.style.opacity === '0') {
+  if (content.style.opacity !== '0') {
     content.animate(
-      collapsibleAnimation,
-      collapsibleOption
-    ).reverse();
-    setTimeout(() => {
-      content.style.opacity = 1;
-    }, 900)
-  } else {
-    content.animate(
-      collapsibleAnimation,
+      contentAnimation,
       collapsibleOption
     );
+    button.animate(
+      buttonAnimation,
+      collapsibleOption
+    );
+    visible.animate(
+      buttonTextAnimation,
+      collapsibleOption
+    );
+    button.disabled = 'true';
+
     setTimeout(() => {
+      visible.style.display = 'none';
+      hidden.style.display = 'inline';
+    }, 700);
+    setTimeout(() => {
+      button.disabled = false;
       content.style.opacity = 0;
+    }, 900)
+  } else if (content.style.opacity === '0') {
+    content.animate(
+      contentAnimation,
+      collapsibleOption
+    ).reverse();
+    button.animate(
+      buttonAnimation,
+      collapsibleOption
+    ).reverse();
+    hidden.animate(
+      buttonTextAnimation,
+      collapsibleOption
+    );
+    button.disabled = 'true';
+
+    setTimeout(() => {
+      hidden.style.display = 'none';
+      visible.style.display = 'inline';
+    }, 700);
+    setTimeout(() => {
+      button.disabled = false;
+      content.style.opacity = 1;
     }, 900)
   }
 });
