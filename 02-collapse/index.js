ba4435bb8851f1a0e43a5b5e3ea61233398d288d
Alexander <renyreny125tt@gmail.com>
2022-08-29 12:03:33 +0300

    feat: add cocollapsible content

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,31 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector('.collapsible__button');
+const content = document.querySelector('.collapsible__content');
+
+let collapsibleAnimation= [
+  {transform: 'translateY(0) rotateX(0deg)', opacity: '1'},
+  {transform: 'translateY(100px) rotateX(360deg)', opacity: '0'}
+]
+
+let collapsibleOption = {
+  duration: 1000
+}
+
+button.addEventListener('click', () => {
+  if (content.style.opacity === '0') {
+    content.animate(
+      collapsibleAnimation,
+      collapsibleOption
+    ).reverse();
+    setTimeout(() => {
+      content.style.opacity = 1;
+    }, 900)
+  } else {
+    content.animate(
+      collapsibleAnimation,
+      collapsibleOption
+    );
+    setTimeout(() => {
+      content.style.opacity = 0;
+    }, 900)
+  }
+});
